f0f3b58d48178a6d2dc11066728cae04dac3937f
convergytics12 <131863819+convergytics12@users.noreply.github.com>
2023-06-02 16:25:14 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -6,11 +6,11 @@ uploaded_files = st.file_uploader("Upload Files", accept_multiple_files=True)
 filelist = []
 
 for file in uploaded_files:
-    #filelist.append(file.name)
+    filelist.append(file.name)
     df1 = pd.read_excel(file)
     st.write(df1)
-#selected_file = st.selectbox("Select a file:", filelist)
-
+selected_file = st.selectbox("Select a file:", filelist)
+st.write(selected_file)
     
         
         
