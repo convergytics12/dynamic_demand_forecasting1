83ab632353bc043cc1998f325067cdd650a4730d
convergytics12 <131863819+convergytics12@users.noreply.github.com>
2023-06-02 16:36:29 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,23 +1,34 @@
 import streamlit as st
 import pandas as pd
 
+ 
+
+ 
+
 uploaded_files = st.file_uploader("Upload Files", accept_multiple_files=True)
 
+ 
+
+ 
+
 filelist = []
-dict={}
-for i in uploaded_files:
-    #dict[i]=i.name
-    st.write(i)
-    
-st.write(dict)
-    
+
+ 
+
+ 
 
 for file in uploaded_files:
-    st.write(file)
+
     filelist.append(file.name)
-    df1 = pd.read_excel(file)
-    st.write(df1)
+
 selected_file = st.selectbox("Select a file:", filelist)
+
+ 
+
+for i in uploaded_files:
+    if(i.name==selected_file):
+        df = pd.read_excel(i)
+        st.dataframe(df)
 
         
         
